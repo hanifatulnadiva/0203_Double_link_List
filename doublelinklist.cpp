8543535ef62c4f9c84bbbf4bcf1ca473f20fa10d
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-03 11:20:10 +0700

    menyisipkan node baru ke dalam list

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -20,4 +20,17 @@ void AddNode(){
     cout << "\nEnter the name of the student: ";
     cin >> newNode -> name;
 
+    if(START == NULL|| newNode -> noMhs <= START -> noMhs){
+        if(START != NULL && newNode -> noMhs == START -> noMhs){
+            cout << "\033[31mDuplicate roll numbers not allowed \033[0m" << endl;
+            return;
+        }
+        newNode -> next = START;
+        if(START != NULL){
+            START -> prev =newNode;
+        }
+        newNode -> prev =NULL;
+        START = newNode;
+    }
+    
 }
