836718dfa73e6ef9459914086215985209bea999
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-03 13:36:53 +0700

    menampilkan pesan untuk mengimputkan nomor identifikasi yang ingin di hapus

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -81,5 +81,8 @@ void deleteNode()
     Node *previous, *current;
     int rollNo;
 
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;
+
     
 }
