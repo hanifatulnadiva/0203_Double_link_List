67011953b94fdfdf2b16bba74e8cad340904e525
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-03 13:52:49 +0700

    menambahkan kondisi if jika node yang ingin dihapus adalah node pertama dalam linked list(start)

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -101,5 +101,14 @@ void deleteNode()
         cout << "\033[31mThe e=record with thw roll number"<< rollNo << "not found\033[0m" << endl;
         return;
     }
+    if ( current == START)
+    {
+        START = START -> next;
+        if (START != NULL)
+        {
+            START ->prev = NULL;
+        }
+    }
+    
     
 }
